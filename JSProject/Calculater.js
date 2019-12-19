6208ad72dde1d9d91907efedc708a85f1740fd5b
caglasualtas <32480738+caglasualtas@users.noreply.github.com>
2019-12-19 13:53:31 +0300

    Add files via upload

---
+++ JSProject/Calculater.js
@@ -0,0 +1,72 @@
+
+class Convertion{
+base2ConvertBase10(number,initialBase){
+    let changeBase = 10;
+    return parseInt(number + '',initialBase).toString(changeBase);
+
+}
+
+base2ConvertBase8(number,initialBase){
+  
+    let changeBase = 8;
+    return parseInt(number + '',initialBase).toString(changeBase);
+
+}
+
+base2ConvertBase16(number,initialBase){
+    let changeBase = 16;
+    return parseInt(number + '',initialBase).toString(changeBase);
+
+}
+base10ConvertBase2(number,initialBase){
+    let changeBase = 2;
+    return parseInt(number + '',initialBase).toString(changeBase);
+
+}
+base10ConvertBase8(number,initialBase){
+    let changeBase = 8;
+    return parseInt(number + '',initialBase).toString(changeBase);
+
+}
+base10ConvertBase16(number,initialBase ){
+    let changeBase = 2;
+    return parseInt(number + '',initialBase).toString(changeBase);
+
+}
+
+base8ConvertBase2(number,initialBase ){
+    let changeBase = 2;
+    return parseInt(number + '',initialBase).toString(changeBase);
+
+}
+
+base8ConvertBase10(number,initialBase ){
+    let changeBase = 2;
+    return parseInt(number + '',initialBase).toString(changeBase);
+
+}
+
+base8ConvertBase16(number,initialBase ){
+    let changeBase = 2;
+    return parseInt(number + '',initialBase).toString(changeBase);
+
+}
+
+base16ConvertBase2(number,initialBase ){
+    let changeBase = 2;
+    return parseInt(number + '',initialBase).toString(changeBase);
+
+}
+
+base16ConvertBase10(number,initialBase ){
+    let changeBase = 2;
+    return parseInt(number + '',initialBase).toString(changeBase);
+
+}
+
+base16ConvertBase8(number,initialBase ){
+    let changeBase = 2;
+    return parseInt(number + '',initialBase).toString(changeBase);
+
+}
+}

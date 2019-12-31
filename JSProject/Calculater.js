90a2bce8ab9b01c9f641dff9ceb073c0b4c5c551
Caglasu <caglasueylulaltas@gmail.com>
2019-12-31 16:15:21 +0300

    updated

--- JSProject/Calculater.js
+++ JSProject/Calculater.js
@@ -528,6 +528,37 @@ function hesap() {
     }
 
 }
+var screen = "";
+function operatorFunx() {
+    if (screen.indexOf("+") != -1) {
+      var numbers = screen.split("+");
+      var x = parseInt(numbers[0], 2);
+      var y = parseInt(numbers[1], 2);
+      var sum = x + y;
+      var ans = sum.toString(2);
+    } else if (screen.indexOf("-") != -1) {
+      var numbers = screen.split("-");
+      var x = parseInt(numbers[0], 2);
+      var y = parseInt(numbers[1], 2);
+      var sub = x - y;
+      var ans = sub.toString(2);
+    } else if (screen.indexOf("*") != -1) {
+      var numbers = screen.split("*");
+      var x = parseInt(numbers[0], 2);
+      var y = parseInt(numbers[1], 2);
+      var mul = x * y;
+      var ans = mul.toString(2);
+    } else if (screen.indexOf("/") != -1) {
+      var numbers = screen.split("/");
+      var x = parseInt(numbers[0], 2);
+      var y = parseInt(numbers[1], 2);
+      var div = x / y;
+      var ans = div.toString(2);
+    }
+    screen = ans;
+    document.getElementById("yazılan").innerHTML = screen;
+  }
+  
 
 
 
